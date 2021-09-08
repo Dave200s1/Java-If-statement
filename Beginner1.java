d3f083daf8fdab6a4ae1902b7af2aece8134acc4
Dave200s1 <81190030+Dave200s1@users.noreply.github.com>
2021-09-08 18:36:18 +0200

    Add files via upload

---
+++ Beginner1.java
@@ -0,0 +1,18 @@
+
+public class Beginner1{
+
+    public static void main(String[]args) {
+        
+        int alter = 10;
+        int mindesAlter = 18;
+        
+        if(alter < mindesAlter){
+            System.out.println("du sollst mindestens 18 Jahre sein um reinzukommen ");
+        }else{
+            System.out.println("Zugriff erlaubt darfst reinkommen");
+        }
+        //System.out.println("Hello Anatoli");
+    }
+}
+
+
